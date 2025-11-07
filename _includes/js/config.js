e3e46c3d7b4a0d207390c5b457ee0f740a4d3364
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-11-07 11:43:08 +0100

    update to config version 3

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,48 +1,100 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
-
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary,
-  fontSize: '16px'
-}});
-
 var siteConfig = {
-  routes: {
-    occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-      route: '/objekter/sog'
+  "version": 3,
+  "pages": [
+    {
+      "id": "occurrenceSearch",
+      "path": "objekter/sog"
+    }
+  ],
+  "disableInlineTableFilterButtons": false,
+  "availableCatalogues": [
+    "OCCURRENCE"
+  ],
+  "dataHeader": {
+    "enableApiPopup": false,
+    "enableInfoPopup": false
+  },
+  "theme": {
+    "primary": themeStyle.colors.primary,
+    "borderRadius": 3,
+    "stickyOffset": "0px"
+  },
+  "maps": {
+    "locale": "en",
+    "mapStyles": {
+      "defaultProjection": "MERCATOR",
+      "defaultMapStyle": "NATURAL",
+      "options": {
+        "ARCTIC": [
+          "NATURAL",
+          "BRIGHT"
+        ],
+        "PLATE_CAREE": [
+          "NATURAL",
+          "BRIGHT"
+        ],
+        "MERCATOR": [
+          "NATURAL",
+          "BRIGHT"
+        ],
+        "ANTARCTIC": [
+          "NATURAL",
+          "BRIGHT"
+        ]
+      }
     }
   },
-  occurrence: {
-    mapSettings: {
-      lat: 0,
-      lng: 0,
-      zoom: 0
+  "languages": [
+    {
+      "code": "en",
+      "localeCode": "en",
+      "label": "English",
+      "default": false,
+      "textDirection": "ltr",
+      "iso3LetterCode": "eng",
+      "cmsLocale": "en-GB",
+      "vocabularyLocale": "en",
+      "gbifOrgLocalePrefix": "",
+      "mapTileLocale": "en"
     },
-    // You probably need help to configure the scope - so just ask
-    // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
-    // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
-    // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: { type: 'equals', key: 'publishingOrg', value: '689b40c4-ff31-4cd0-83a5-a7a828f1cd92' }, 
-    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
-    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
-  },
-  maps: {
-    locale: 'en',
-    defaultProjection: 'MERCATOR',
-    defaultMapStyle: 'NATURAL',
-    mapStyles: {
-      ARCTIC: ['NATURAL', 'BRIGHT'],
-      PLATE_CAREE: ['NATURAL', 'BRIGHT'],
-      MERCATOR: ['NATURAL', 'BRIGHT'],
-      ANTARCTIC: ['NATURAL', 'BRIGHT']
+    {
+      "code": "false",
+      "localeCode": "en",
+      "label": "English",
+      "default": true,
+      "textDirection": "ltr",
+      "iso3LetterCode": "eng",
+      "cmsLocale": "en-GB",
+      "vocabularyLocale": "en",
+      "gbifOrgLocalePrefix": "",
+      "mapTileLocale": "en"
     }
-  }
-};
+  ],
+  "messages": {},
+  "occurrenceSearch": {
+    "scope": {
+      "type": "equals",
+      "key": "publishingOrg",
+      "value": "689b40c4-ff31-4cd0-83a5-a7a828f1cd92"
+    },
+    "mapSettings": {
+      "lat": 0,
+      "lng": 0,
+      "zoom": 0
+    }
+  },
+  "collectionSearch": {},
+  "institutionSearch": {},
+  "datasetSearch": {},
+  "publisherSearch": {},
+  "literatureSearch": {}
+}
 
 // example of a language specific route overwrite
-if (pageLang === 'en')  {
-  siteConfig.routes.occurrenceSearch.route = '/occurrence/search';
+if (pageLang === 'en') {
+  siteConfig.pages = {
+    "id": "occurrenceSearch",
+    "path": "occurrence/search"
+  }
+
 }
